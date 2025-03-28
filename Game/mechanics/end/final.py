133d2ec45c97c0be73a0c8f70ca0d6b2c32fc7f5
Duo48-Kirill <kirilldoshinsky@yandex.ru>
2025-03-28 12:12:41 +0300

    Update final.py

--- Game/mechanics/end/final.py
+++ Game/mechanics/end/final.py
@@ -7,11 +7,11 @@ class Ending:
         for line in file:
             line: str = line.replace("Алексей", name)
             print(line)
-            time.sleep(3)
+            time.sleep(5)
 
     def second_end(name: str) -> None:
         file: list[str] = open("mechanics/end/end_2.txt", encoding="utf-8").readlines()
         for line in file:
             line: str = line.replace("Алексей", name)
             print(line)
-            time.sleep(3)
+            time.sleep(5)
